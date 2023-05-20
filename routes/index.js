e803f0abaf1ac329e962135c12c6676d071cf9c5
KoHyunsu <sce06147@gmail.com>
2023-05-20 23:11:36 +0900

    [Init] express.js

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const k8s = require('@kubernetes/client-node');
+const kc = new k8s.KubeConfig();
+kc.loadFromFile('./config');
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+router.get('/nodes', (req, res, next) => {
+  k8sApi.listNode().then((response) => {
+    res.status(200).json(response.body.items);
+  }).catch((error) => {
+    res.status(500).send(error);
+  })
+});
+
+router.get('/pods', (req, res, next) => {
+  k8sApi.listPodForAllNamespaces().then((response) => {
+    res.status(200).json(response.body.items);
+  }).catch((error) => {
+    res.status(500).send(error);
+  });
+});
+
+router.get('/pods/:name/:namespace', (req, res, next) => {
+  k8sApi.readNamespacedPodLog(req.params.name, req.params.namespace).then(() => {
+    res.status(200).json(response.body.items);
+  }).catch((error) => {
+    res.status(500).json(error.body);
+  })
+});
+
+module.exports = router;
